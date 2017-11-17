99517c920f6807b6a7a81c537e270aefd4292b85
s3610887 <s3610887@rmit.edu.vn>
2017-11-17 18:54:21 +0700

    public shaming

---
+++ lab4/Ex9.java
@@ -0,0 +1,18 @@
+package lab4;
+
+import java.util.GregorianCalendar;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        GregorianCalendar date = new GregorianCalendar();
+        System.out.println(date.get(GregorianCalendar.YEAR));
+        System.out.println(date.get(GregorianCalendar.MONTH));
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH));
+        date.setTimeInMillis(1234567898765L);
+        System.out.println(date.get(GregorianCalendar.YEAR));
+        System.out.println(date.get(GregorianCalendar.MONTH));
+        System.out.println(date.get(GregorianCalendar.DAY_OF_MONTH));
+    }
+}
+
+
